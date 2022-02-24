da3324ccc7275f698c65ad08119a4d6aff766336
pithuene <pitsimonhuene@t-online.de>
2022-02-24 09:53:28 +0100

    Add details to oplist_print

--- ops/oplist.c
+++ ops/oplist.c
@@ -41,9 +41,65 @@ void oplist_clear(oplist_t *oplist) {
 }
 
 void oplist_print(oplist_t *oplist) {
+  printf("Oplist:\n");
   int i = 0;
   for (oplist_item_t *item = oplist->head; item != NULL; item = item->next) {
-    printf("%d: Optype %d\n", i, *item->op);
+    switch(*item->op) {
+      case optype_nop: {
+        printf("%d: Nop\n", i);
+        break;
+      }
+      case optype_offset: {
+        op_offset_t *op = (op_offset_t*) item->op;
+        printf("%d: Offset %f / %f\n", i, op->offset.x, op->offset.y);
+        break;
+      }
+      case optype_begin_path: {
+        printf("%d: BeginPath\n", i);
+        break;
+      }
+      case optype_fill_color: {
+        op_fill_color_t *op = (op_fill_color_t*) item->op;
+        printf("%d: FillColor %d %d %d %d\n", i, op->color.r, op->color.g, op->color.b, op->color.a);
+        break;
+      }
+      case optype_fill: {
+        printf("%d: Fill\n", i);
+        break;
+      }
+      case optype_rect: {
+        op_rect_t *op = (op_rect_t*) item->op;
+        printf("%d: Rect width: %f height: %f\n", i, op->width, op->height);
+        break;
+      }
+      case optype_circle: {
+        op_circle_t *op = (op_circle_t*) item->op;
+        printf("%d: Circle radius: %f\n", i, op->radius);
+        break;
+      }
+      case optype_text: {
+        op_text_t *op = (op_text_t*) item->op;
+        printf("%d: Text\n", i);
+        printf("%d:   font: %s\n", i, op->font->name);
+        printf("%d:   size: %f\n", i, op->size);
+        if (op->end) {
+          int string_len = op->end - op->string;
+          printf("%d:   string: %.*s\n", i, string_len, op->string);
+        } else {
+          printf("%d:   string: %s\n", i, op->string);
+        }
+        break;
+      }
+      case optype_register_input_area: {
+        op_register_input_area_t *op = (op_register_input_area_t*) item->op;
+        printf("%d: RegisterInputArea %f / %f with id %lu\n", i, op->dimensions.x, op->dimensions.y, op->area_id);
+        break;
+      }
+      default: {
+        printf("%d: PRINT NOT IMPLEMENTED FOR OPTYPE %d\n", i, *item->op);
+        break;
+      }
+    }
     i++;
   }
 }
