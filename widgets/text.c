013bcb28d50ea0c83e1dd39d63913e8d70fd846d
pithuene <pitsimonhuene@t-online.de>
2022-02-24 10:18:02 +0100

    Fallback to fallback font when none is given in text widget

--- widgets/text.c
+++ widgets/text.c
@@ -4,9 +4,14 @@ point_t text(AppContext *app, point_t constraints, text_t *conf) {
   op_begin_path(&app->oplist);
   op_fill_color(&app->oplist, conf->color);
 
-  point_t bounds = text_bounds(app->vg, conf->size, conf->font, conf->content, conf->content_end);
+  Font *font = conf->font;
+  if (!font) {
+    font = &app->font_fallback;
+  }
 
-  op_text(&app->oplist, conf->size, conf->font, conf->content, conf->content_end);
+  point_t bounds = text_bounds(app->vg, conf->size, font, conf->content, conf->content_end);
+
+  op_text(&app->oplist, conf->size, font, conf->content, conf->content_end);
 
   return bounds;
 }
